b01e9219256027e395e80225ae9cce8d18b5c746
Marine Houze <m.houzedelaulnoit@gmail.com>
2016-06-14 14:29:07 +0200

    'modify route '

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
 
   mount Attachinary::Engine => "/attachinary"
 
+  devise_for :users, only: :omniauth_callbacks, controllers: {omniauth_callbacks: 'users/omniauth_callbacks'}
+
   scope '(:locale)', locale: /fr|en/ do
-    devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+    devise_for :users, skip: :omniauth_callbacks
     resources :users, :teachers, :lessons, :bookings
     root to: 'pages#home'
   end
