2a03cbd12850e034b8d67eac0e0c9c225b6324b5
Alexis Quesney <alexis.quesney@ecota.co>
2016-06-14 14:45:33 +0200

    pundit suite final

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
-  include Pundit
-  after_action :verify_authorized, except: [:home]
+  # Disable devise
+  skip_before_action: :authenticate_user!
+  # Disable Pundit
+  skip_after_action :verify_authorized
 
   def home
   end
