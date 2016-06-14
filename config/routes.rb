f8c5e1f012d5ccaa7b58137eb56aecc44804b526
Marine Houze <m.houzedelaulnoit@gmail.com>
2016-06-14 12:19:07 +0200

    add attachinary route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  root to: 'pages#home'
 
   mount Attachinary::Engine => "/attachinary"
+
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  root to: 'pages#home'
 
 end
