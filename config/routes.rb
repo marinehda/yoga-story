e44f323be910bb098b633f0bd3d3dd4a9c83a79b
Nath Meriaux <nath.meriaux@gmail.com>
2016-06-14 12:20:07 +0200

    correction traduction fr manquante dans fichier devise/shared/_links.html.reb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: 'pages#home'
 
-  scope '(:locale)', locale: /fr|es/ do
+  scope '(:locale)', locale: /fr|en/ do
+    devise_for :users
+    root to: 'pages#home'
     resources :users, :teachers, :lessons, :bookings
   end
   # The priority is based upon order of creation: first created -> highest priority.
