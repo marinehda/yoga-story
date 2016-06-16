cc8d5e097f1a29f9fa8695d2d489e8cab7603d89
Nath Meriaux <nath.meriaux@gmail.com>
2016-06-16 13:44:32 +0200

    thème installé et footer traduit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,3 +5,14 @@
 //= require jquery-fileupload/basic
 //= require cloudinary/jquery.cloudinary
 //= require attachinary
+//
+//= require custom
+//= require froogaloop2.min
+//= require headroom
+//= require jquery-migrate.min
+//= require jquery.backstretch.min
+//= require jquery.easing.1.3.min
+//= require modernizr.custom.97074
+//= require parallax.min
+//= require smoothscroll
+//= require vimeo
