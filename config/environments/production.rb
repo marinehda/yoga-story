1d6137e423e70b97962958eba491a376de6d9d63
Alexis Quesney <alexis.quesney@ecota.co>
2016-06-17 18:38:48 +0200

    fix smtp config postmark

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   config.action_mailer.default_url_options = { host: "http://yoga-story.herokuapp.com/" }
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.action_mailer.delivery_method = :smtp
   # Code is not reloaded between requests.
   config.cache_classes = true
 
