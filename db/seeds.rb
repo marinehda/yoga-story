c2e5488fa8f62eb6e1a2e9a7c38e727caeab2b22
Marine Houze <m.houzedelaulnoit@gmail.com>
2016-06-18 19:42:11 +0200

    seeds done

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,31 +3,32 @@ require 'faker'
 # Run using rake db:reset rather than rake db:seed to not break ids logic in this file
 # When modifying validators in model, amend this file
 
-#User.delete_all
+User.delete_all
 Lesson.delete_all
+Booking.delete_all
 
-# (1..10).each do |i|
-#   user = User.new(
-#     email: 'user' + i.to_s + '@yogastory.com',
-#     password: 'aaaaaa'
-#     )
-#   user.save!
-#   p 'user' + i.to_s
-# end
-#
-# user1 = User.first
-# user1.is_admin = true
-# user1.save
-#
-# (11..15).each do |i|
-#   user = User.new(
-#     email: 'user' + i.to_s + '@yogastory.com',
-#     password: 'aaaaaa',
-#     type: 'Teacher'
-#     )
-#   user.save!
-#   p 'teacher' + i.to_s
-# end
+(1..10).each do |i|
+  user = User.new(
+    email: 'user' + i.to_s + '@yogastory.com',
+    password: 'aaaaaa'
+    )
+  user.save!
+  p 'user' + i.to_s
+end
+
+user1 = User.first
+user1.is_admin = true
+user1.save
+
+(11..15).each do |i|
+  user = User.new(
+    email: 'user' + i.to_s + '@yogastory.com',
+    password: 'aaaaaa',
+    type: 'Teacher'
+    )
+  user.save!
+  p 'teacher' + i.to_s
+end
 
 (1..20).each do |i|
   lesson = Lesson.new(
