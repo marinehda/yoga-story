7fbe26041932c98379580124533f5cb8ce5b918b
Alexis Quesney <alexis.quesney@ecota.co>
2016-06-21 14:17:20 +0200

    stripe works with pundit, amazing...

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,30 +1,33 @@
 class PaymentsController < ApplicationController
   before_action :set_booking
+  after_action :verify_authorized
 
   def new
+    authorize @booking
   end
 
   def create
-  @amount_cents = @booking.amount_cents
+    authorize @booking
+    @amount_cents = @booking.amount_cents
 
-  customer = Stripe::Customer.create(
-    source: params[:stripeToken],
-    email:  params[:stripeEmail]
-  )
+    customer = Stripe::Customer.create(
+      source: params[:stripeToken],
+      email:  params[:stripeEmail]
+    )
 
-  charge = Stripe::Charge.create(
-    customer:     customer.id,   # You should store this customer id and re-use it.
-    amount:       @amount_cents, # in cents
-    description:  "Payment for lesson #{@booking.lesson_sku} for booking #{@booking.id}",
-    currency:     'eur'
-  )
+    charge = Stripe::Charge.create(
+      customer:     customer.id,   # You should store this customer id and re-use it.
+      amount:       @amount_cents, # in cents
+      description:  "Payment for lesson #{@booking.lesson_sku} for booking #{@booking.id}",
+      currency:     'eur'
+    )
 
-  @booking.update(payment: charge.to_json, state: 'paid')
-  redirect_to order_path(@booking)
+    @booking.update(payment: charge.to_json, payment_state: 'paid')
+    redirect_to booking_path(@booking)
 
-  rescue Stripe::CardError => e
-  flash[:error] = e.message
-  redirect_to new_order_payment_path(@booking)
+    rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to new_booking_payment_path(@booking)
   end
 
   private
