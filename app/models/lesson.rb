b7322d3b5d4f8b5ffb73fa0a777bdca80c5c5ae4
Alexis Quesney <alexis.quesney@ecota.co>
2016-06-21 19:08:07 +0200

    validator lesson start_date

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -5,4 +5,11 @@ class Lesson < ActiveRecord::Base
   after_validation :geocode, if: :address_changed?
   validates_presence_of :start_date, :hours, :min_students, :max_students, :price, :name, :description, :address
   monetize :price_cents
+  validate :start_date_in_future
+
+  def start_date_in_future
+    if self.start_date < DateTime.now
+      errors.add(:start_date, "Start date should be before end date")
+    end
+  end
 end
