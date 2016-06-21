e1fc332140af5ade2078283ecd0c66c431d3b15b
Alexis Quesney <alexis.quesney@ecota.co>
2016-06-21 12:54:09 +0200

    payment form and controller

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,4 +1,35 @@
 class PaymentsController < ApplicationController
+  before_action :set_booking
+
   def new
+  end
+
+  def create
+  @amount_cents = @booking.amount_cents
+
+  customer = Stripe::Customer.create(
+    source: params[:stripeToken],
+    email:  params[:stripeEmail]
+  )
+
+  charge = Stripe::Charge.create(
+    customer:     customer.id,   # You should store this customer id and re-use it.
+    amount:       @amount_cents, # in cents
+    description:  "Payment for lesson #{@booking.lesson_sku} for booking #{@booking.id}",
+    currency:     'eur'
+  )
+
+  @booking.update(payment: charge.to_json, state: 'paid')
+  redirect_to order_path(@booking)
+
+  rescue Stripe::CardError => e
+  flash[:error] = e.message
+  redirect_to new_order_payment_path(@booking)
+  end
+
+  private
+
+  def set_booking
+    @booking = Booking.where(payment_state: 'pending').find(params[:booking_id])
   end
 end
