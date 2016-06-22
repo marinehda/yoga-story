029cda097b3595a574ad330a984f190501329aa0
arrabatur <fix@doum.it>
2016-06-22 16:10:53 +0200

    fixed map

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,13 +4,7 @@
 //= require jquery-migrate.min
 //= require jquery.backstretch.min
 //= require jquery.easing.1.3.min
-//= require jquery.counterup.min
-//= require jquery.flexslider-min
-//= require jquery.mb.YTPlayer.min
-//= require jquery.sticky
 //= require bootstrap-sprockets
-//= require jasny-bootstrap.min
-//= require jqBootstrapValidation
 //= require jquery-fileupload/basic
 //= require cloudinary/jquery.cloudinary
 //= require attachinary
@@ -18,13 +12,6 @@
 //= require custom
 //= require froogaloop2.min
 //= require headroom
-//= require contact_me
-//= require cube-portfolio
-//= require one-page
-//= require owl.carousel.min
-//= require pace.min
-//= require revolution-custom
-//= require waypoints.min
 
 //= require parallax.min
 //= require smoothscroll
@@ -34,8 +21,4 @@
 //= require wow
 
 //= require_tree .
-
-
-
-
 
